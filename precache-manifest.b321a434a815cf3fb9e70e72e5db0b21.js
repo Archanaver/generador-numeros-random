1f6166b1e075d6e8dcc219b34c6feb127a746c8f
Archana <A01335895@itesm.mx>
2022-04-14 18:30:48 -0500

    Updates

---
+++ precache-manifest.b321a434a815cf3fb9e70e72e5db0b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29267f8b369d7b176f2d",
+    "url": "/generador-numeros-random/static/css/main.0e5b59f7.chunk.css"
+  },
+  {
+    "revision": "29267f8b369d7b176f2d",
+    "url": "/generador-numeros-random/static/js/main.29267f8b.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/generador-numeros-random/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "794b132895d930b248f5",
+    "url": "/generador-numeros-random/static/js/2.794b1328.chunk.js"
+  },
+  {
+    "revision": "7bda6a4363321713d40f",
+    "url": "/generador-numeros-random/static/js/runtime~main.7bda6a43.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/generador-numeros-random/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5d66f74720b64ba9c72ce39bf8cba5ec",
+    "url": "/generador-numeros-random/index.html"
+  }
+];
